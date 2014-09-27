0bcb9d912ee6d3a74d16c906606f0c66c77de92d
Ryan D'souza <dsouzarc@gmail.com>
2014-09-27 18:17:43 -0400

    Carrier constants

--- src/Variables.java
+++ src/Variables.java
@@ -1,4 +1,13 @@
+/** To hold common variables */
 
 public class Variables {
-
+	
+	//CARRIERS
+	public static final String VERIZON = "@vtext.com";
+	public static final String ATT = "@txt.att.net";
+	public static final String TMOBILE = "@tmomail.net";
+	public static final String VIRGINMOBILE = "@vmobl.com";
+	public static final String CINGULAR = "@cingularme.com";
+	public static final String SPRINT = "@messaging.sprintpcs.com";
+	public static final String NEXTEL = "@messaging.nextel.com";
 }
